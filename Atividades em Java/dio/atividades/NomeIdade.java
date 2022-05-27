29ce19189d864ad129fa971a3bc2242f76459296
johnclecio <johnclecio@icloud.com>
2022-05-26 21:15:34 -0300

    atividades em Java

---
+++ Atividades em Java/dio/atividades/NomeIdade.java
@@ -0,0 +1,26 @@
+package dio.atividades;
+
+import java.util.Scanner;
+
+public class NomeIdade {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true){
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+
+            System.out.println("idade: ");
+            idade = scan.nextInt();
+
+
+
+        }
+        System.out.println("continua aqui....");
+
+    }
+}
---
+++ Atividades em Java/dio/atividades/ParEImpar.java
@@ -0,0 +1,33 @@
+package dio.atividades;
+
+import java.util.Scanner;
+
+public class ParEImpar {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int quantNumeros;
+    int numero;
+    int quantPares = 0, quantImpares = 0;
+
+    System.out.println("Quantiddade de números: ");
+    quantNumeros = scan.nextInt();
+
+    int count = 0;
+    do {
+        System.out.println("Número: ");
+        numero = scan.nextInt();
+
+        if(numero % 2 == 0) quantPares++;
+        else quantImpares++;
+
+        count++;
+
+    }while( count < quantNumeros);
+
+        System.out.println("Quantidades de Par: " + quantPares);
+        System.out.println("Quantidades de Impar: " + quantImpares);
+
+
+    }
+}
